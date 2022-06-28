4b68264c47b3cbbdd2248cfac4d856731e1726ce
kdsabella <kensabella04@gmail.com>
2022-06-28 09:26:57 +0800

    portfolio

---
+++ js/main.js
@@ -0,0 +1,108 @@
+// Navigation bar effects on scroll
+window.addEventListener("scroll", function() {
+    const header = this.document.querySelector('header');
+    header.classList.toggle('sticky', this.window.scrollY > 0);
+});
+
+// Modal Services
+
+const serviceModal = document.querySelectorAll('.service-modal');
+const learnmoreBtns = document.querySelectorAll('.learn-more-btn');
+const modalcloseBtns = document.querySelectorAll('.modal-close-btn');
+
+var modal = (modalCLick) => {
+    serviceModal[modalCLick].classList.add('active');
+};
+
+learnmoreBtns.forEach((learnmoreBtn, i) => {
+    learnmoreBtn.addEventListener('click', () => {
+        modal(i);
+    })
+});
+
+modalcloseBtns.forEach((modalcloseBtn) => {
+    modalcloseBtn.addEventListener('click', () => {
+        serviceModal.forEach((modalView) => {
+            modalView.classList.remove('active');
+        });
+    });
+});
+
+// to top scroll
+const scrollTopBtn = document.querySelector(".scrollToTop-btn");
+
+window.addEventListener("scroll", function() {
+    scrollTopBtn.classList.toggle("active", window.scrollY > 500);
+});
+
+scrollTopBtn.addEventListener("click", () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+});
+
+// Navigation active on page scroll
+window.addEventListener("scroll", () => {
+    const sections = document.querySelectorAll("section");
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current => {
+        let sectionHeight = current.offsetHeight;
+        let sectionTop = current.offsetTop - 50;
+        let id = current.getAttribute("id");
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector(".nav-items a[href*=" + id + "]").classList.add("active");
+        } else {
+            document.querySelector(".nav-items a[href*=" + id + "]").classList.remove("active");
+        }
+    })
+
+
+})
+
+
+// light and Dark theme
+
+const themeBtn = document.querySelector(".theme-btn");
+
+themeBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-theme");
+    themeBtn.classList.toggle("sun");
+
+
+    localStorage.setItem("save-theme", getCurrentTheme());
+    localStorage.setItem("saved-icon", getCurrentIcon());
+
+});
+
+const getCurrentTheme = () => document.classList.contains("dark-theme") ? "dark" : "light";
+const getCurrentIcon = () => document.classList.contains("sun") ? "sub" : "moon";
+
+const saveTheme = localStorage.getItem("saved-theme");
+const saveIcon = localStorage.getItem("saved-icon");
+
+if (saveTheme) {
+    document.body.classList[saveTheme === "dark" ? "add" : "remove"]("dark-theme");
+    themeBtn.classList[saveIcon === "sun" ? "add" : "remove"]("sun");
+}
+
+// Navigation Button Toggle
+const menuBtn = document.querySelector(".nav-menu-btn");
+const closeBtn = document.querySelector(".nav-close-btn");
+const navigation = document.querySelector(".navigation");
+const navItems = document.querySelectorAll(".nav-items a");
+
+
+navItems.forEach((navItem) => {
+    navItem.addEventListener("click", () => {
+        navigation.classList.remove("active");
+    })
+})
+
+menuBtn.addEventListener("click", () => {
+    navigation.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+    navigation.classList.remove("active");
+});
